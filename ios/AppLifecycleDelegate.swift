44e681cc9d7b1ba1dba0a10ac57699bf8167c9de
Ethan <ethan.park@sodacrew.com>
2022-08-24 10:23:46 +0900

    v0.6.0 add expo-modules-core

---
+++ ios/AppLifecycleDelegate.swift
@@ -0,0 +1,15 @@
+import ExpoModulesCore
+import PayPalCheckout
+
+public class AppLifecycleDelegate: ExpoAppDelegateSubscriber {
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+      let config = CheckoutConfig(
+        clientID: "AZ7GEyCVqTQTeMd-ew_27mPT1uoNPzYUJ3Szk0yw-OiF7CchhrzwhEaU_sm0hrgO6EvtlzDAcvI9KRF8",
+            environment: .sandbox
+        )
+
+    Checkout.set(config: config)
+        return true
+    }
+}
