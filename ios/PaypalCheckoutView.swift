b69ca1d652a1e8e5d0b31ea1f6d38db4ee9c6303
Ethan <ethan.park@sodacrew.com>
2022-08-26 11:06:58 +0900

    Update working version

--- ios/PaypalCheckoutView.swift
+++ ios/PaypalCheckoutView.swift
@@ -1,59 +1,64 @@
-
 import Foundation
 import UIKit
 import PayPalCheckout
 
-
 @objc(PaypalCheckoutView)
 class PaypalCheckoutView : UIView {
-var paymentButton: PayPalButton?
-
-  @objc var paymentId: NSString?;
-  @objc var onMessage: RCTDirectEventBlock?;
-
-
-      override func didSetProps(_ changedProps: [String]!) {
-        if let paymentButton = self.paymentButton {
-            paymentButton.removeFromSuperview()
+    
+    var paymentButtonContainer: PaymentButtonContainer?
+    
+    @objc var paymentId: NSString?;
+    @objc var onMessage: RCTDirectEventBlock?;
+    
+    override func didSetProps(_ changedProps: [String]!) {
+        
+        if let paymentButtonContainer = self.paymentButtonContainer {
+            paymentButtonContainer.removeFromSuperview()
         }
         
-        self.paymentButton = PayPalButton()
-        configurePayPalCheckout()
-        if let paymentButton = self.paymentButton {
-            self.addSubview(paymentButton)
+   
+        self.paymentButtonContainer = PaymentButtonContainer(
+            payPalButtonUIConfiguration: PayPalButtonUIConfiguration(attributes: PaymentButtonAttributes(size: PaymentButtonSize(rawValue:300) ?? .expanded)),
+            payLaterButtonUIConfiguration: PayLaterButtonUIConfiguration.init()
+        )
+        
+
+        if let paymentButtonContainer = self.paymentButtonContainer {
+            paymentButtonContainer.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+            
+            self.addSubview(paymentButtonContainer)
+            NSLayoutConstraint.activate(
+                [
+                    paymentButtonContainer.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+                    paymentButtonContainer.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                ]
+            )
         }
-      }
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-  }
-
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-
-    override func layoutSubviews() {
-        if let paymentButton = self.paymentButton {
-            paymentButton.frame = self.bounds
-        }
-    }
-
-  func configurePayPalCheckout() {
+    
+    func configurePayPalCheckout() {
         Checkout.setCreateOrderCallback { createOrderAction in
             createOrderAction.set(orderId: self.paymentId! as String)
         }
-
         Checkout.setOnApproveCallback { _approve in
             self.onMessage!(["message": "approved"])
         }
-          Checkout.setOnCancelCallback {
-              self.onMessage!(["message": "cancelled"])
-          }
-
-    Checkout.setOnErrorCallback { error in
-        self.onMessage!(["error": error])
+        Checkout.setOnCancelCallback {
+            self.onMessage!(["message": "cancelled"])
+        }
+        
+        Checkout.setOnErrorCallback { error in
+            self.onMessage!(["error": error])
+        }
     }
-  }
 }
