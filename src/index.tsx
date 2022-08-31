273cc24d5ec2b35d83461f0b5adf3031376833a2
Ethan <ethan.park@sodacrew.com>
2022-09-01 00:47:08 +0900

    v1.5.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
 import {
-  requireNativeComponent,
-  StyleProp,
-  ViewStyle,
+  // requireNativeComponent,
+  // StyleProp,
+  // ViewStyle,
   NativeModules,
 } from 'react-native';
-import React from 'react';
+// import React from 'react';
 
 interface TriggerArgs {
   paymentId: string;
   onMessage: (error: any, result: any) => void;
 }
-interface NativeProps {
-  paymentId: string;
-  onMessage: (event: any) => void;
-  style?: StyleProp<ViewStyle>;
-}
+// interface NativeProps {
+//   paymentId: string;
+//   onMessage: (event: any) => void;
+//   style?: StyleProp<ViewStyle>;
+// }
 
-const PaypalCheckoutViewNative =
-  requireNativeComponent<NativeProps>('PaypalCheckoutView');
+// const PaypalCheckoutViewNative =
+//   requireNativeComponent<NativeProps>('PaypalCheckoutView');
 
-export const PaypalCheckoutButton = (props: NativeProps) => {
-  return <PaypalCheckoutViewNative {...props} />;
-};
+// export const PaypalCheckoutButton = (props: NativeProps) => {
+//   return <PaypalCheckoutViewNative {...props} />;
+// };
 
 export const triggerPaypalCheckout = (args: TriggerArgs) =>
   NativeModules.PaypalCheckoutTrigger.triggerPaypalCheckout(
